cad701c73bcf654f19d3b92295ccf7583fc67263
SANKET GHAYAL <sanketghayal3@gmail.com>
2025-09-06 10:01:01 +0530

    Add files via upload

---
+++ WhatsApp Web App/models/chats.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+const chatSchema = new mongoose.Schema({
+    from: {
+        type: String,
+        required: true
+    },
+    to: {
+        type: String,
+        required: true
+    },
+    msg: {
+        type: String
+    },
+    created_at: {
+        type: Date
+    }
+});
+
+const Chat = new mongoose.model("Chat" , chatSchema);
+
+module.exports = Chat;
