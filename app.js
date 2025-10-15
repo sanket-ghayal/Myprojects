d327993c7400616983b8ce84c1b1ed4780a001ff
Sanket Ghayal <sanketghayal3@gmail.com>
2025-10-15 19:03:58 +0530

    Updated logic in app.js

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const mongo_Url = 'mongodb://127.0.0.1:27017/wanderlust'
-const dbUrl = process.env.ATLASDB_URL; 
+const mongo_Url = 'mongodb://127.0.0.1:27017/wanderlust'
+// const dbUrl = process.env.ATLASDB_URL; 
 
 main().then((res) => {
     console.log("successfully connected to DB");
